ddc0094bd5362c075556880fddcf096dad783f24
MrAdisson <cedric.adisson@gmail.com>
2023-05-09 21:38:16 +0200

    new logger with chalk

--- src/hooks/useLogger.tsx
+++ src/hooks/useLogger.tsx
@@ -1,15 +1,27 @@
 //A LOGGER HOOK TO LOG THE STATE OF THE COMPONENTS
 
-import { useEffect } from 'react';
+import chalk from 'chalk';
 
-export const useLogger = (component: () => JSX.Element) => {
-    const { name } = component;
-    useEffect(() => {
-        console.log(`%c${name} - COMPONENT MOUNTED`, 'color: green');
-        return () => {
-            console.log(`%c${name} - COMPONENT UNMOUNTED`, 'color: green');
-        };
-    });
+export type LogType = 'info' | 'warn' | 'error' | 'success';
+
+export const useLogger = () => {
+    const logger = {
+        info: (args: any, origin?: string) =>
+            console.log(chalk.grey(`[${new Date().toLocaleString()}]`), chalk.blue(origin ? `[${origin}]` : '[INFO]'), chalk.grey(':'), typeof args === 'string' ? chalk.blueBright(args) : args),
+        warn: (args: any, origin?: string) =>
+            console.log(chalk.grey(`[${new Date().toLocaleString()}]`), chalk.yellow(origin ? `[${origin}]` : '[WARN]'), chalk.grey(':'), typeof args === 'string' ? chalk.yellowBright(args) : args),
+
+        error: (args: any, origin?: string) =>
+            console.log(chalk.grey(`[${new Date().toLocaleString()}]`), chalk.red(origin ? `[${origin}]` : '[ERROR]'), chalk.grey(':'), typeof args === 'string' ? chalk.redBright(args) : args),
+        success: (args: any, origin?: string) =>
+            console.log(chalk.grey(`[${new Date().toLocaleString()}]`), chalk.green(origin ? `[${origin}]` : '[SUCCESS]'), chalk.grey(':'), typeof args === 'string' ? chalk.greenBright(args) : args)
+    };
+
+    const log = (args: any, type?: LogType) => {
+        return type ? logger[type](args) : logger.info(args);
+    };
+
+    return { log, logger };
 };
 
 // Path: src\hooks\useLogger.tsx
