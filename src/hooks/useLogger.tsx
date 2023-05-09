066aeb8c0a6bc49c9e3a60cbd3e8d107b3cc4d18
MrAdisson <cedric.adisson@gmail.com>
2023-05-09 21:57:46 +0200

    1.0.8 no more chalk

--- src/hooks/useLogger.tsx
+++ src/hooks/useLogger.tsx
@@ -1,26 +1,25 @@
 //A LOGGER HOOK TO LOG THE STATE OF THE COMPONENTS
 
-import chalk from 'chalk';
-
 export type LogType = 'info' | 'warn' | 'error' | 'success';
 
 export const useLogger = () => {
     const logger = {
-        info: (args: any, origin?: string) =>
-            console.log(chalk.grey(`[${new Date().toLocaleString()}]`), chalk.blue(origin ? `[${origin}]` : '[INFO]'), chalk.grey(':'), typeof args === 'string' ? chalk.blueBright(args) : args),
-        warn: (args: any, origin?: string) =>
-            console.log(chalk.grey(`[${new Date().toLocaleString()}]`), chalk.yellow(origin ? `[${origin}]` : '[WARN]'), chalk.grey(':'), typeof args === 'string' ? chalk.yellowBright(args) : args),
-
-        error: (args: any, origin?: string) =>
-            console.log(chalk.grey(`[${new Date().toLocaleString()}]`), chalk.red(origin ? `[${origin}]` : '[ERROR]'), chalk.grey(':'), typeof args === 'string' ? chalk.redBright(args) : args),
-        success: (args: any, origin?: string) =>
-            console.log(chalk.grey(`[${new Date().toLocaleString()}]`), chalk.green(origin ? `[${origin}]` : '[SUCCESS]'), chalk.grey(':'), typeof args === 'string' ? chalk.greenBright(args) : args)
+        info: (args: any) => {
+            console.log(`%c${new Date().toLocaleTimeString()}: [INFO] - `, 'color: #00a3cc', args);
+        },
+        warn: (args: any) => {
+            console.log(`%c${new Date().toLocaleTimeString()}: [WARN] - `, 'color: #ffcc00', args);
+        },
+        error: (args: any) => {
+            console.log(`%c${new Date().toLocaleTimeString()}: [ERROR] - `, 'color: #ff3300', args);
+        },
+        success: (args: any) => {
+            console.log(`%c${new Date().toLocaleTimeString()}: [SUCCESS] - `, 'color: #00cc00', args);
+        }
     };
-
     const log = (args: any, type?: LogType) => {
         return type ? logger[type](args) : logger.info(args);
     };
-
     return { log, logger };
 };
 
