93814a86b1341f6ffd8d7c5fced18f2cd8257702
MrAdisson <cedric.adisson@gmail.com>
2023-05-09 20:41:40 +0200

    new useLogger imple

--- src/hooks/useLogger.tsx
+++ src/hooks/useLogger.tsx
@@ -2,19 +2,21 @@
 
 import { useEffect } from 'react';
 
-export const useLogger = (componentName: string, props: any) => {
-    useEffect(() => {
-        console.log(`${componentName} PROPS: `, props);
-    }, [props]);
+export const useLogger = () => {
+    //GET CALLER COMPONENT INFORMATION DYNAMICALLY:
+    const getComponentName = (component: any) => {
+        return component.displayName || component.name || 'Component';
+    };
+
+    //LOG THE STATE OF THE COMPONENTS
 
     useEffect(() => {
-        console.log(`${componentName} MOUNTED`);
+        console.log(`%c${getComponentName(this)}: Mounted`, 'color: green');
+
         return () => {
-            console.log(`${componentName} UNMOUNTED`);
+            console.log(`%c${getComponentName(this)}: Unmounted`, 'color: red');
         };
     }, []);
-
-    return null;
 };
 
 // Path: src\hooks\useLogger.tsx
